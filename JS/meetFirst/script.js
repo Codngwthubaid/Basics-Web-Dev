21edb164c8c4cd799ea9154e61cc5b38a52c133c
Codngwthubaid <codngwthubaid@gmail.com>
2024-10-08 19:56:27 +0530

    meetFirst is done for JS

---
+++ JS/meetFirst/script.js
@@ -0,0 +1,89 @@
+// console.log("Hello Ankit ...")
+// alert("Im here to learn JS ....")
+
+// var , let , const 
+// var Name = "Ankit-BCA"
+// console.log(Name);
+// const moreName = "Teerthanker Mahaveer University"
+// console.log(moreName);
+// moreName = "TMU"
+// console.log(moreName);
+
+// For String
+// let anotherName = "Ubaid"
+// // console.log(anotherName);
+// let dataCheckStr = typeof(anotherName)
+// console.log(dataCheckStr);
+
+// For Number
+// let num = 90
+// let dataCheckNum = typeof(num)
+// console.log(dataCheckNum);
+
+// For Boolean
+// console.log(typeof(true));
+// console.log(typeof(false));
+
+// For Object
+// const obj = {
+//     Name : "Ankit",
+//     Course : "BCA",
+//     College : "TMU"
+// }
+// key : Value
+
+// console.log(obj);
+// console.log(typeof(obj));
+
+// If-Else conditional Operation
+// if (condition) {
+// Code
+// } 
+// else {
+// Code
+// }
+
+
+// Pre-Taken Value
+// let age = 20
+// console.log("Age is :", age);
+
+// User inserted Value
+// const age = prompt("Enter Your Age : ")
+// console.log("Age is :", age);
+
+// if (age < 18) {
+//     console.log("Sorry , you are not eligible for driving !!!");
+// }
+// else {
+//     console.log("You are eligiable for driving");
+// }
+
+
+// 1-10
+// let num = 1;
+// console.log(num);
+// console.log(num+1);
+// console.log(num+2);
+// console.log(num+3);
+// console.log(num+4);
+// console.log(num+5);
+// console.log(num+6);
+// console.log(num+7);
+// console.log(num+8);
+// console.log(num+9);
+
+// let num = 1
+// while (condition) {
+//    #code 
+// }
+
+// while (num <= 10) {
+//     console.log(num);
+//     num++
+// }
+
+
+
+
+
